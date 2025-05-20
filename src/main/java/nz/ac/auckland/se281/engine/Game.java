31d22e05bac132e173f7187f424e59062280cdbc
kylerosser <39887097+kylerosser@users.noreply.github.com>
2025-05-21 10:54:20 +1200

    Modify play method to print an appropriate message for the first round

--- src/main/java/nz/ac/auckland/se281/engine/Game.java
+++ src/main/java/nz/ac/auckland/se281/engine/Game.java
@@ -5,15 +5,21 @@ import nz.ac.auckland.se281.cli.MessageCli;
 
 public class Game {
   public static String AI_NAME = "HAL-9000";
+  private int thisRoundNumber;
+  private int numberOfRounds;
 
   public Game() {}
 
   public void newGame(Difficulty difficulty, int numRounds, String[] options) {
     String namePlayer = options[0];
+    this.thisRoundNumber = 1;
+    this.numberOfRounds = numRounds;
     MessageCli.WELCOME_PLAYER.printMessage(namePlayer);
   }
 
-  public void play() {}
+  public void play() {
+    MessageCli.START_ROUND.printMessage(this.thisRoundNumber, this.numberOfRounds);
+  }
 
   public void showStats() {}
 }
